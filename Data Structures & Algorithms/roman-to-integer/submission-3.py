4cccd1230e9e9b0025b8f3cc991c1d78b52979b8
eswarsde <eswar.sde@gmail.com>
2026-07-16 17:10:04 -0500

    Add: roman-to-integer - submission-3

---
+++ Data Structures & Algorithms/roman-to-integer/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        symbol_to_val = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,           
+        }
+
+        # Roman numerals are usually written largest to smallest from left to right
+
+        result = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and symbol_to_val[s[i]] < symbol_to_val[s[i+1]]:
+                result-=symbol_to_val[s[i]] 
+            else:
+                result+=symbol_to_val[s[i]]
+        return result
