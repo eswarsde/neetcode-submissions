c9ef7e41aaf0977a842de4c02ebc2a0bea4cbec4
eswarsde <eswar.sde@gmail.com>
2026-07-16 22:31:03 -0500

    Add: remove-duplicates-from-sorted-array - submission-2

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-2.py
@@ -0,0 +1,78 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # [1,1,2,3,4]
+        #    w r
+
+        if len(nums) == 1:
+            return 1
+        
+        write = 1
+
+        for read in range(1, len(nums)):
+            if nums[read] != nums[read -1]:
+                nums[write] = nums[read]
+                write+=1
+
+        return write
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # write = 1 #If the array is not empty, the first element (index = 0) is always unique.
+        # for read in range(1, len(nums)):
+        #     if nums[read]!=nums[write-1]: # the value at read pointer is a new unique value
+        #         nums[write] = nums[read]
+        #         write+=1
+                
+        # return write
+
+        
