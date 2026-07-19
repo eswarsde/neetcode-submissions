7c9fb2eedc13c8abdcbc160e7b5a070a562f7d4b
eswarsde <eswar.sde@gmail.com>
2026-07-18 22:59:35 -0500

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # hashmap
+        # loop over and find the one
+
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                seen.remove(num)
+            else:
+                seen.add(num)
+        return seen.pop()
+
+
+        
