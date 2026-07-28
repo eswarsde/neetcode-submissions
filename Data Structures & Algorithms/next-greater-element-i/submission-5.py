d75b14a442def65181dd2809acb83dcd7c65c5e1
eswarsde <eswar.sde@gmail.com>
2026-07-28 18:19:00 -0500

    Add: next-greater-element-i - submission-5

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-5.py
@@ -0,0 +1,70 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        # Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+
+        # Output: [-1,3,-1]
+
+        # Naive way: nested loop 
+
+        # for each num in nums 1
+        #   * find the match index in nums 2
+        #   * then continue right to find the greater element
+          # if no return -1
+
+    #  # Store the final answers in the same order as nums1.
+    #     result = []
+    
+    #     for target in nums1:
+
+    #         answer = -1
+            
+    #         found_target = False
+            
+    #         for num in nums2:
+
+    #             if not found_target:
+    #                 if num == target:
+    #                     found_target = True
+    #             else:
+    #                 if num > target:
+    #                     answer = num
+    #                     break
+            
+    #         result.append(answer)
+        
+    #     # Return all answers in nums1 order.
+    #     return result
+
+    #    # Time complexity: O(len(nums1) * len(nums2))
+    #    # Space Complexity: O(len(nums1))
+
+
+       # Idea 2:
+        next_greater_lookup = {}
+        stack = []
+        # Pre-porcess nums2
+        for num in nums2:
+            while stack and num > stack[-1]:
+                smaller = stack.pop()
+                next_greater_lookup[smaller] = num
+            
+            stack.append(num)
+        # decreasing order from bottom to top. When a new element is larger than the stack top,
+        # we pop elements and record the current element as their next greater. We only push elements that are in nums1 since those are the only ones we need answers for.
+        while stack:
+            leftover = stack.pop()
+            next_greater_lookup[leftover] = -1
+        
+        ans = []
+
+        for num in nums1:
+            ans.append(next_greater_lookup[num])
+
+        return ans
+
+            
+
+
+
+
