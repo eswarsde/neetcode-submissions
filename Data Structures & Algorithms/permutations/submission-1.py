c6000f7e315816f679e6c1d4b12f2613137dc9f6
eswarsde <eswar.sde@gmail.com>
2026-08-03 11:23:41 -0500

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        num_len = len(nums)
+
+        path = []
+        result = []
+        def dfs_backtracking():
+            # base case
+            if len(path) == num_len:
+                # append carefully - Append a COPY of the current path
+                result.append(path[:])
+                return
+            # choices -> choose -> explore -> unchoose
+
+            for choice in nums:
+                # can't choose the same number again as per the problem 
+                if choice in path:
+                    continue
+                path.append(choice)
+                dfs_backtracking()
+                path.pop()
+        dfs_backtracking()
+        return result
