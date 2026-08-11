0fe61ee386f6dd28035bac70a6d668757cfb46ef
eswarsde <eswar.sde@gmail.com>
2026-08-11 14:58:58 -0500

    Add: find-pivot-index - submission-3

---
+++ Data Structures & Algorithms/find-pivot-index/submission-3.py
@@ -0,0 +1,46 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+
+    # That is a very common question, and understanding this "plus one" "(len(nums) + 1)" trick is the key to mastering the prefix sum pattern.
+    #The main reason we make the prefix array one element larger than the input array is to cleanly handle edge cases and avoid out-of-bounds errors without writing extra if/else statements.
+
+    #If you look at the core formula in the range_sum(), you'll notice it relies on this offset. To get the sum of everything up to index i, we need to subtract the sum of everything before our starting point. If our starting point is index 0, we have to subtract prefix[0]
+        n = len(nums)
+        prefix_sum = [0] * (n + 1)
+
+        for idx, num in enumerate(nums): # T: O(n), S: O(n)
+            prefix_sum[idx + 1] = prefix_sum[idx] + num
+
+        # the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+        for idx, num in enumerate(nums):
+            left = prefix_sum[idx] # everything strictly to the left of i
+            right = prefix_sum[n] - prefix_sum[idx + 1]
+
+            if left == right:
+                return idx
+
+        return -1
+# T: O(n), S: O(n)
+
+
+# Apprach 2# why keep an entire prefix sum array, 
+# instead if we have the total_sum of the array and running_sum until that index
+# left = running_sum
+# right = total - left - current_number
+
+        # total_sum = sum(nums)
+        # left = 0  # This takes the place of our prefix_sum array
+        
+        # for i, num in enumerate(nums):
+        #     # Calculate the sum of everything strictly to the right
+        #     right = total_sum - left - num
+            
+        #     # Check for pivot
+        #     if left == right:
+        #         return i
+                
+        #     # Update the left running sum for the next index
+        #     left += num
+            
+        # return -1
+
