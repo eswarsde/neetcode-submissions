e5cb8884df7830f26caac2e4316f9d1dede59e62
eswarsde <eswar.sde@gmail.com>
2026-08-11 19:52:29 -0500

    Add: products-of-array-discluding-self - submission-11

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-11.py
@@ -0,0 +1,77 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+
+        # start with 1 not zero because zero can make all products zero
+        result = [1] * (len(nums))
+
+        running_product_prefix = 1
+        for idx, num in enumerate(nums):
+            # Except Self: Store and then Update
+            result[idx] = running_product_prefix
+            running_product_prefix *= num
+
+
+        running_product_suffix = 1
+        for idx in range(n-1, -1, -1):
+            # Except Self: Store and then Update
+            result[idx] *= running_product_suffix
+            running_product_suffix *= nums[idx]
+        
+        return result
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+        
